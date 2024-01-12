0145ae67579e3ac973e88825b2787668aff79d68
theArctesian <stephen.d.okita@gmail.com>
2024-01-12 13:30:34 +0800

    pooing and peeing

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -10,9 +10,12 @@
 		{#each members as person}
 			<div class="flex flex-col card p-3 gap-2">
 				<!-- <img src="/group-pic.jpeg" alt="{person.name} image" /> -->
-				<div
+				<!-- <div
 					class="bg-[url({person.img})] hover:bg-[url({person.imgSilly})] bg-cover h-48 w-48 rounded m-auto"
-				/>
+				/> -->
+
+				<img src="{person.img}" alt="{person.name} image" class="object-contain h-48 w-48 rounded m-auto"
+				>
 				<h1 class="text-center dark:text-white font-bold h2">{person.name}</h1>
 				<h1 class="text-center dark:text-white h5">{person.title}</h1>
 				<a class=" flex justify-center content-center">
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,18 @@
-<h1>Project System</h1>
-<h3>Projects</h3>
-<p>
-	2-3 high-impact projects with external sponsors/partnerships Topics selected by project managers
-	and approved by the Project VP (Atharva) Emphasis on the product delivered
-</p>
-<h3>Initaves</h3>
+<script>
+	import Fade from "$lib/Fade.svelte";
+	import projects from "./project.json"
+</script>
+<div class="h-full flex ab flex-col px-1/4 py-1/4 mt-36">
+	<Fade>
+		<div class="flex flex-col gap-7">
+			<!-- <img src="/group-pic.jpeg" class="m-auto absolute -z-10" /> -->
+			<h1 class="text-6xl dark:text-white text-center font-bold">Our Projects System</h1>
+			<div class="flex px-10">
+				<p class="h5 text-secondary-500 dark:text-secondary-100 leading-10 text-center">
+					Every year we have 3-4 projects blah blah blah Rohit fill please
+				</p>
+			</div>
+		</div>
+	</Fade>
+</div>
+	
